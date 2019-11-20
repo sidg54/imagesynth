1eeaa8c1cfac98d919d6daeb704573751459977c
siddarthganguri <ganguri.s@husky.neu.edu>
2019-11-19 19:39:26 -0500

    Added flask_cors to requirements.

--- api/app.py
+++ api/app.py
@@ -1,22 +1,12 @@
 r'''
 '''
 # third party imports
+import pickle
+import pandas as pd
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+# internal imports
 from infer import Infer
 
 app = Flask(__name__)
-
-
-def get_inference(inp, model_name):
-    infer = Infer(model_name)
-    inference = infer.infer(inp=inp)
-    return inference
-
-
-@app.route('/predict', methods=['POST'])
-def infer():
-    if request.method == 'POST':
-        # first, get the numeric from user input
-        number = request.files['file']
-        class_id, class_name = get_inference(inp=number)
-        return jsonify({'class_id': class_id, 'class_name': class_name})
